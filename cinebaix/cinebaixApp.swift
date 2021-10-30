ebeb2aece4ca6adb77e0cda1538308df4fc36133
Alex Hernández Velasco <alex.hernandez.velasco@nttdata.com>
2021-10-30 13:53:54 +0200

    Initial Commit

---
+++ cinebaix/cinebaixApp.swift
@@ -0,0 +1,17 @@
+//
+//  cinebaixApp.swift
+//  cinebaix
+//
+//  Created by Alex Hernandez Velasco on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct cinebaixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
